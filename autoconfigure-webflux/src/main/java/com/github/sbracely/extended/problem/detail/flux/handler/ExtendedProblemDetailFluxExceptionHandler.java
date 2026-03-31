c4ddfce00be72dc6c4db4a4fc1d9a0ef70019d01
sbrace <sbrace0822@gmail.com>
2026-04-01 00:51:49 +0800

    clear flux test

--- autoconfigure-webflux/src/main/java/com/github/sbracely/extended/problem/detail/flux/handler/ExtendedProblemDetailFluxExceptionHandler.java
+++ autoconfigure-webflux/src/main/java/com/github/sbracely/extended/problem/detail/flux/handler/ExtendedProblemDetailFluxExceptionHandler.java
@@ -1,10 +1,7 @@
 package com.github.sbracely.extended.problem.detail.flux.handler;
 
-import com.github.sbracely.extended.problem.detail.response.Error;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler;
 
@@ -13,13 +10,4 @@ public class ExtendedProblemDetailFluxExceptionHandler extends ResponseEntityExc
 
     private static final Logger log = LoggerFactory.getLogger(ExtendedProblemDetailFluxExceptionHandler.class);
 
-    private Error ObjectErrorConvertToError(ObjectError objectError) {
-        Error error = new Error();
-        if (objectError instanceof FieldError fieldError) {
-            error.setField(fieldError.getField());
-        }
-        error.setMessage(objectError.getDefaultMessage());
-        error.setType(Error.Type.PARAMETER);
-        return error;
-    }
 }
