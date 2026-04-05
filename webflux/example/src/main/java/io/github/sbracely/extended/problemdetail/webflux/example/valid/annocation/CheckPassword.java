031505b7fe8551639475e143e6e30beccb74c5fd
sbrace <sbrace0822@gmail.com>
2026-04-05 19:47:49 +0800

    add module flux-example

---
+++ webflux/example/src/main/java/io/github/sbracely/extended/problemdetail/webflux/example/valid/annocation/CheckPassword.java
@@ -0,0 +1,20 @@
+package io.github.sbracely.extended.problemdetail.webflux.example.valid.annocation;
+
+import io.github.sbracely.extended.problemdetail.webflux.example.valid.validator.CheckPasswordValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = CheckPasswordValidator.class)
+public @interface CheckPassword {
+    String message() default "Check password error";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
---
+++ webflux/example/src/test/java/io/github/sbracely/extended/problemdetail/webflux/example/WebfluxExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sbracely.extended.problemdetail.webflux.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
