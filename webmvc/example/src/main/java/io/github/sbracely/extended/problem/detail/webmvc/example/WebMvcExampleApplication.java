331820cd1b11bd2fd00454dc937b3f6f249dfa40
sbrace <sbrace0822@gmail.com>
2026-04-07 21:43:45 +0800

    rename package name

---
+++ webmvc/example/src/main/java/io/github/sbracely/extended/problem/detail/webmvc/example/WebMvcExampleApplication.java
@@ -0,0 +1,13 @@
+package io.github.sbracely.extended.problem.detail.webmvc.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMvcExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebMvcExampleApplication.class, args);
+    }
+
+}
