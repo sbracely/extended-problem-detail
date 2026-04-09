95cef88164c047528fdc1938a8c53dc0af328dde
sbrace <sbrace0822@gmail.com>
2026-04-09 23:29:20 +0800

    update openapi title

--- webflux/example/src/main/java/io/github/sbracely/extended/problem/detail/webflux/example/config/FluxOpenApiConfiguration.java
+++ webflux/example/src/main/java/io/github/sbracely/extended/problem/detail/webflux/example/config/FluxOpenApiConfiguration.java
@@ -33,8 +33,8 @@ public class FluxOpenApiConfiguration {
     OpenAPI webFluxExampleOpenApi() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Extended Problem Detail WebFlux Example API")
-                        .description("Example Spring WebFlux endpoints that demonstrate Extended Problem Detail responses.")
+                        .title("Extended Problem Detail Boot 4 WebFlux Example API")
+                        .description("Example Spring Boot 4 WebFlux endpoints that demonstrate Extended Problem Detail responses.")
                         .version("1.1.0"))
                 .servers(List.of(new Server()
                         .url("/")
